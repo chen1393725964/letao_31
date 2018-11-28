6bce7a7faaacf34de82766735f30a95f8367c886
chen1393725964 <1393725964@qq.com>
2018-11-28 11:27:37 +0800

    公共的退出功能和拦截完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,4 +1,6 @@
 
+
+
 // 1、进度基本使用
 $(document).ajaxStart(function(){
   NProgress.start();
@@ -6,5 +8,42 @@ $(document).ajaxStart(function(){
 $(document).ajaxStop(function(){
   setTimeout(function(){
     NProgress.done();
-  },2000)
+  },500)
+})
+ 
+
+$(function(){
+  // 3个公共能
+  // 1、左侧菜单栏切换
+   $(".category").click(function(){
+     $(this).next().stop().slideToggle();
+   })
+  //  2、左侧侧边栏隐藏和显示
+   $(".ico_left").click(function(){
+     $(".lt_aside").toggleClass("hidemenu");
+     $(".lt_main").toggleClass("hidemenu");
+     $(".lt_topbar").toggleClass("hidemenu");
+   })
+
+  //  3、退出功能 模态框（）
+    //  (1)点击右上角的按钮显示模态框
+     $(".ico_right").click(function(){
+       $("#logoutModal").modal("show")  
+     })
+    //  (2)点击模态框退出按钮 完成退出功能(像后台发送ajax请求)
+      $("#logoutBtn").click(function(){
+        //  发送ajax请求让后台销毁当前用户的状态
+        $.ajax({
+            // 这个地址就是处理你要退出的数据
+          url:"/employee/employeeLogout",
+          dateType:"json",
+          type:"get",
+          success:function(info){
+            // console.log(info);
+            if(info.success){
+              location.href="login.html";
+            }
+          }
+        })
+      })
 })
--- public/back/js/isLogin.js
+++ public/back/js/isLogin.js
@@ -1,11 +1,16 @@
 // 登录拦截
-// $.ajax({
-//    url:"/employee/employeeLogout",
-//    type:"get",
-//    dateType:"json",
-//    success:function(info){
-//     console.log(info);
-    
-//    }
-
-// })
+$.ajax({
+  url:"/employee/checkRootLogin",
+  type:"get",
+  dateType:"json", 
+  success:function(info){
+    // console.log(info);
+    if(info.error==400){
+      // 表示未登陆的 拦截跳转回到login页面
+      location.href="login.html"
+    }
+     if(info.success){
+       console.log("当前用户登录成功");
+     }
+  } 
+})
